68d29ce41004fe2fd18fbca627329c23cf3bcdc4
Frederik Hermans <frederik.ac.hermans@gmail.com>
2019-07-05 16:02:51 +0100

    Play sound when a certain number is dialed

--- rotary.py
+++ rotary.py
@@ -3,19 +3,27 @@
 from __future__ import print_function
 
 import RPi.GPIO as GPIO
+import pygame
 import time
 
 # Getting warning "RuntimeWarning: A physical pull up resistor is fitted on this channel!" for PIN2.
 # * Even though I've specified GPIO.RISING, I'm still sporadically
 #   getting callbacks on what I believe should be a falling edge.
 # * Bouncetime of 75ms seems to work flawlessly on the pulse detector
+# * Would be nicer to use pygame.mixer.Sound() objects, but they cannot be MP3 (need WAV or OGG)
 
 pulses = 0
+number = ''
 
 def movement_ended(channel):
-    global pulses
+    global pulses, number
     if pulses > 0:
-        print(pulses % 10, end='')
+        number += str(pulses % 10)
+        print(number)
+        if number.endswith('235898'):
+            print('Playing sound...')
+            pygame.mixer.music.play(loops=0, start=53)
+
     pulses = 0
 
 def pulse(channel):
@@ -33,6 +41,10 @@ def main():
     GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(2, GPIO.RISING, callback=movement_ended, bouncetime=200)
     GPIO.add_event_detect(3, GPIO.BOTH, callback=pulse, bouncetime=75)
+
+    pygame.mixer.init()
+    pygame.mixer.music.load("sounds/final_countdown.mp3")
+    pygame.mixer.music.set_volume(1.0)
 
     print('Press CTRL+C to quit.')
     try:

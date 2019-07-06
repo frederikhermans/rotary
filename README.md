fc0bbc76739fab867c4f0ec8f94886a04c9e2378
Frederik Hermans <frederik.ac.hermans@gmail.com>
2019-07-06 12:17:27 +0100

    Add rudimentary support for hanging up.

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ Hook up rotary dialer to GPIO pins 2 (movement indicator) and 3 (pulse
 indicator). Attach earpiece to the headphone jack.
 
 Create a `sounds/` subdirectory and populate it with sound files. If
-there's a file called 258844.mp3, it will be called when 258844 is
+there's a file called 258844.mp3, it will be played when 258844 is
 dialed.
--- rotary.py
+++ rotary.py
@@ -14,14 +14,17 @@ RE_SOUND_FILE = re.compile(r'^(?P<number>[\d]+)\....$')
 
 class Dialer(object):
 
-    def __init__(self, on_number_dialled, movement_end_channel=2, pulse_channel=3):
+    def __init__(self, on_number_dialled, movement_end_channel=2, pulse_channel=3, hangup_channel=4):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(movement_end_channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(pulse_channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(hangup_channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.add_event_detect(
             movement_end_channel, GPIO.RISING, callback=self._on_movement_end, bouncetime=200)
         GPIO.add_event_detect(
             pulse_channel, GPIO.BOTH, callback=self._on_pulse, bouncetime=75)
+        GPIO.add_event_detect(
+            hangup_channel, GPIO.RISING, callback=self._on_hangup, bouncetime=500)
 
         self._pulses = 0
         self._dialed = ''
@@ -29,12 +32,20 @@ class Dialer(object):
 
     def _on_movement_end(self, _):
         if self._pulses > 0:
+            print(self._pulses % 10, end='')
             self._dialed += str(self._pulses % 10)
             self._on_number_dialled(self._dialed)
         self._pulses = 0
 
     def _on_pulse(self, _):
         self._pulses += 1
+
+    def _on_hangup(self, _):
+        if len(self._dialed):
+            print('\r{}\r'.format('_' * len(self._dialed)), end='')
+        self._dialed = ''
+        # This shouldn't be here, but be part of the player
+        pygame.mixer.music.stop()
 
 
 class Player(object):
